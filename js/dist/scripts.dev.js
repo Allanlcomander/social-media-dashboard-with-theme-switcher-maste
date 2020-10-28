d3b4e89df59ebae4c319b4cf2be3293a9320c4c9
allanlcomander <andala63@gmail.com>
2020-10-28 14:06:57 +0300

    start mobile editing

--- js/dist/scripts.dev.js
+++ js/dist/scripts.dev.js
@@ -2,16 +2,14 @@
 //     var element = document.body;
 //    element.classList.toggle("dark-mode");
 // };
-
 // var darkModee = function () {
 //     var element = document.body;
 //     element.classList.toggle("header");
 // }
-
-
-
 // $(document).ready(function() {
 //     $(".button").click(function() {
 //        $("body").addClass("darkMode"); 
 //     });
 // });
+"use strict";
+//# sourceMappingURL=scripts.dev.js.map
